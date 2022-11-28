1c810492722bd11e00806fd5fb23a2048ce93b98
Luciana <l.lopesdealmeida25@gmail.com>
2022-11-28 17:11:25 +0000

    pet class added :D

--- js/script.js
+++ js/script.js
@@ -1,21 +1,53 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     // loading 2D environment
     let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d"); 
+    let ctx = canvas.getContext("2d");
 
     // obstacle class
     class Obstacle {
         constructor(height, width) {
             this.height = height;
             this.width = width;
-          }
+        }
     }
 
-    bed =  new Obstacle(500,400);
+    class Pet {
+        constructor(petChoice, x, y, petW, petH) {
+            this.petChoice = petChoice;
+            this.x = x;
+            this.y = y;
+            this.petW = petW;
+            this.petH = petH;
+         }
+        drawPet(x, y){
+            var petUrl = "../media/" + this.petChoice + "-lofi.png";
+            const petimg = new Image();
+            petimg.src = petUrl;
+            petimg.onload = () => {
+                ctx.drawImage(petimg, x, y, this.petW, this.petH);
+            }
+         }
+    }
 
-    var petChoice;
-    petChoice = "dog";
-    var petUrl = "../media/"+petChoice+"-lofi.png";
+    pet = new Pet("dog", canvas.width/2, canvas.height/2, 50, 35);
+    pet.drawPet(canvas.width/2,canvas.height/2);
+
+    bed = new Obstacle(500, 400);
+
+    // var petChoice;
+    // petChoice = "dog";
+    // var petUrl = "../media/" + petChoice + "-lofi.png";
+
+    // // pet init coordinates
+    // var pet.x = canvas.width / 2;
+    // var pet.y = canvas.height / 2;
+    // const pet = new Image();
+    // pet.src = petUrl;
+    // let w = 50; // pet image width
+    // let h = 35; // pet image height
+    // pet.onload = () => {
+    //     ctx.drawImage(pet, pet.x, pet.y, w, h);
+    // }
 
     // var petUrl = function() { 
     //     var petChoice;
@@ -30,20 +62,10 @@ window.addEventListener('load', function() {
     // window.addEventListener("onClick", nextScreen, false);
     //////////////////// Pet Choice ///////////////////////
     /////////////////////////// LEVEL 1 //////////////////////////////////////////////////////////////////
-    
-    // pet init coordinates
-    var petX=canvas.width/2;
-    var petY=canvas.height/2;
-    const pet = new Image();
-    pet.src = petUrl;
-    let w = 50; // pet image width
-    let h = 35; // pet image height
-    pet.onload = () => {
-        ctx.drawImage(pet, petX, petY, w, h);
-    }
+
 
     //checks if the pet is inside the canvas area
-    function inCanvas(x,y){
+    function inCanvas(x, y) {
         return x >= 0 && x < canvas.width && y >= 0 && y < canvas.height;
     }
 
@@ -64,51 +86,51 @@ window.addEventListener('load', function() {
     //     }
     // }
 
-    function move(evt){
+    function move(evt) {
         let step = 5;
         let dx = 5; //how many px pet moves
         let dy = 5;
         // console for checking numbers
         console.log('Key code: ' + evt.keyCode);
-        console.log('x: ' + petX);
-        console.log('y: ' + petY);
+        console.log('x: ' + pet.x);
+        console.log('y: ' + pet.y);
 
-        if (evt.keyCode == 39 && inCanvas(petX + w + 3, petY)) { // right
-            petX += dx;
+        if (evt.keyCode == 39 && inCanvas(pet.x + pet.petW + 3, pet.y)) { // right
+            pet.x += dx;
         }
-        else if (evt.keyCode == 37 && inCanvas(petX + 5, petY)) { // left
-            petX -= dx;
+        else if (evt.keyCode == 37 && inCanvas(pet.x + 5, pet.y)) { // left
+            pet.x -= dx;
         }
-        else if (evt.keyCode == 38 && inCanvas(petX + 5, petY)) { // up
-            petY -= dy;
+        else if (evt.keyCode == 38 && inCanvas(pet.x + 5, pet.y)) { // up
+            pet.y -= dy;
         }
-        else if (evt.keyCode == 40 && inCanvas(petX + 5, petY + h)) { // down
-            petY += dy;
+        else if (evt.keyCode == 40 && inCanvas(pet.x + 5, pet.y + pet.petH)) { // down
+            pet.y += dy;
         }
         // if the pos satisfies incanvas it will draw pet in updated pos
-            
+
         //clears past pet and draws a new one in the updated pos
-        ctx.clearRect(petX-5,petY-5,w+10,h+10);
+        ctx.clearRect(pet.x - 5, pet.y - 5, pet.petW + 10, pet.petH + 10);
         // try to either rotate pet picture when walking or change url path to different angles
         // ctx.scale(-1, 1);
-        ctx.drawImage(pet, petX, petY, w, h);
-        
-     }
+        pet.drawPet(pet.x, pet.y);
+
+    }
 
     //  function interact(evt){
     //     if (evt.keyCode == 73 && inInteractionArea(x,y)){ 
-    //         if (petX - 20< key.x < petX + 20 || petY -20 < key.y < petY + 20){ // arbitary numbers, test to see if works well and adjust
+    //         if (pet.x - 20< key.x < pet.x + 20 || pet.y -20 < key.y < pet.y + 20){ // arbitary numbers, test to see if works well and adjust
     //             addToInventory("key");
     //         }
-    //         else if (petX - 20< food.x < petX + 20 || petY -20 < food.x < petY + 20) {
+    //         else if (pet.x - 20< food.x < pet.x + 20 || pet.y -20 < food.x < pet.y + 20) {
     //             eat;
     //         }
     //     }
     //  }
-     //moves pet when arrow keys are used
-     window.addEventListener("keydown", move, false);
+    //moves pet when arrow keys are used
+    window.addEventListener("keydown", move, false);
     //  window.addEventListener("onClick", petUrl, false);
     //  window.addEventListener("onClick", interact, false);
 
-     return 1;
+    return 1;
 });
