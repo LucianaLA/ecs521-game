a10a09e1bfdfba5066ce34f73299106afbea3ba3
Luciana <l.lopesdealmeida25@gmail.com>
2022-12-12 02:31:51 +0000

    added new sprites

--- js/script.js
+++ js/script.js
@@ -12,30 +12,30 @@ window.addEventListener('load', function () {
     let inventory = []; //fill with item eg [key,food,food], [food,key]
 
     //item (for inventory) class
-    class Item{
-        constructor(itemName){
+    class Item {
+        constructor(itemName) {
             //item type? if item == health, if item == key etc
             itemName = this.itemName;
         }
-        addToInventory(itemName){
+        addToInventory(itemName) {
             inventory = inventory.push(itemName);
         }
-        removeFromInventory(itemName){ //remove first instance of item
-            for(let i=0; i<inventory.length; i++){
-                if(inventory[i]==itemName){
+        removeFromInventory(itemName) { //remove first instance of item
+            for (let i = 0; i < inventory.length; i++) {
+                if (inventory[i] == itemName) {
                     inventory = inventory.splice(i);
                     break;
                 }
             }
         }
-        cleanInventory(){
+        cleanInventory() {
             //when you switch levels, 
             inventory.length = 0;
         }
-        viewInventory(){
+        viewInventory() {
             //return inventory array
         }
-        checkItem(itemName){
+        checkItem(itemName) {
             //searches array for key when prompted by door, if yes, remove from array and open door
             //searches array for food when prompted by user, if yes remove from array and increase health
         }
@@ -43,14 +43,14 @@ window.addEventListener('load', function () {
 
     // obstacle class
     class Obstacle {
-        constructor(x,y,width,height,name) {
+        constructor(x, y, width, height, name) {
             this.width = width;
             this.height = height;
             this.x = x;
             this.y = y;
-            this.name=name;
+            this.name = name;
         }
-        drawObstacle(){
+        drawObstacle() {
             // ctx.rect(this.x,this.y,this.width,this.height);
             // ctx.fill();
             // console.log(this.x);
@@ -64,14 +64,35 @@ window.addEventListener('load', function () {
             }
             return furnitureimg;
         }
-        obstacleArea(x,y){              //read ctx canvas documentation
-            return x > this.x && 
-            x < this.x + this.width && 
-            y > this.y && 
-            y < this.y + this.height;
+        obstacleArea(x, y) {              //read ctx canvas documentation
+            return x > this.x &&
+                x < this.x + this.width &&
+                y > this.y &&
+                y < this.y + this.height;
         }
     }
 
+    // sprite ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    let dog1WalkUrl = "../media/1dog/Walk.png";
+    let dog1IdleUrl = "../media/1dog/Idle.png";
+    let cat3WalkUrl = "../media/3cat/Walk.png";
+    let sprite = new Image();
+    let counter = 0,
+        frame_width = 48,
+        frame_height = 48;
+    petWalk = function (x, y, spriteType) {
+        sprite.src = spriteType;
+        window.requestAnimationFrame(animate);
+        function animate() {
+            let frame = Math.floor(counter % 6);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(sprite, frame * frame_width, 0, frame_width, frame_height, x, y, frame_width, frame_height);
+            counter = counter + .25;
+
+            window.requestAnimationFrame(animate);
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
     class Pet {
         constructor(petChoice, x, y, petW, petH) {
             this.petChoice = petChoice;
@@ -97,26 +118,20 @@ window.addEventListener('load', function () {
             petimg2.onload = () => {
                 ctx.drawImage(petimg2, x, y, this.petW, this.petH);
             }
-
             return petimg2;
         }
 
-        animPet(){
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            // bed.drawObstacle();
-            ctx.drawImage(bed.drawObstacle(), bed.x, bed.y,bed.width,bed.height);
-            // this.drawPet(this.x, this.y);
-            ctx.drawImage(this.drawPet1(), this.x, this.y,this.petW,this.petH); //DOG NO LONGER BLINKS!!!!!!!!!!!!!!
-            ctx.clearRect(0,0,canvas.width,canvas.height); //trying animation frame by frame (inneficient way)
-            for (let i=0;i<obstacleArr.length;i++){
-                ctx.drawImage(obstacleArr[i].drawObstacle(),obstacleArr[i].x,obstacleArr[i].y,obstacleArr[i].width,obstacleArr[i].height);
+        animPet() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            petWalk(this.x, this.y,dog1WalkUrl);
+            ctx.clearRect(0, 0, canvas.width, canvas.height); //trying animation frame by frame (inneficient way)
+            for (let i = 0; i < obstacleArr.length; i++) {
+                ctx.drawImage(obstacleArr[i].drawObstacle(), obstacleArr[i].x, obstacleArr[i].y, obstacleArr[i].width, obstacleArr[i].height);
             }
             // ctx.drawImage(bed.drawObstacle(), bed.x, bed.y,bed.width,bed.height);
-            ctx.drawImage(this.drawPet2(), this.x, this.y,this.petW,this.petH);
-            console.log(this.drawPet1());
-            console.log(this.drawPet2());
+            petWalk(this.x, this.y,dog1IdleUrl);
         }
-        petMove = (evt)=> {
+        petMove = (evt) => {
             let step = 3;
             let dx = 3; //how many px pet moves
             let dy = 3;
@@ -126,7 +141,7 @@ window.addEventListener('load', function () {
             console.log('pet y: ' + this.y);
             let lastx = this.x;
             let lasty = this.y;
-    
+
             //inCanvas position check////////
             if (evt.keyCode == 39 && inCanvas(this.x + this.petW, this.y)) { // right
                 this.x += dx;
@@ -134,17 +149,17 @@ window.addEventListener('load', function () {
             else if (evt.keyCode == 37 && inCanvas(this.x - step, this.y)) { // left
                 this.x -= dx;
             }
-            else if (evt.keyCode == 38 && inCanvas(this.x, this.y-step)) { // up
+            else if (evt.keyCode == 38 && inCanvas(this.x, this.y - step)) { // up
                 this.y -= dy;
             }
             else if (evt.keyCode == 40 && inCanvas(this.x + step, this.y + this.petH)) { // down
                 this.y += dy;
             }
             // if the pos satisfies inCanvas it will draw pet in updated pos////////
-    
+
             //clears past pet and draws a new one in the updated pos
-            for (let i=0;i<obstacleArr.length;i++){
-                if (obstacleArr[i].obstacleArea(this.x,this.y)) {
+            for (let i = 0; i < obstacleArr.length; i++) {
+                if (obstacleArr[i].obstacleArea(this.x, this.y)) {
                     this.x = lastx;
                     this.y = lasty;
                 }
@@ -171,25 +186,25 @@ window.addEventListener('load', function () {
 
     let obstacleArr = new Array();
 
-    const bed = new Obstacle(0, 0, 50, 65,"bed");
+    const bed = new Obstacle(0, 0, 50, 65, "bed");
     obstacleArr[0] = bed;
     bed.drawObstacle();
 
-    const drawer = new Obstacle(50, 0, 10, 10,"drawer");
+    const drawer = new Obstacle(50, 0, 10, 10, "drawer");
     obstacleArr[1] = drawer;
     drawer.drawObstacle();
 
-    const desk = new Obstacle(70, 0, 40, 30,"desk");
+    const desk = new Obstacle(70, 0, 40, 30, "desk");
     obstacleArr[2] = desk;
     desk.drawObstacle();
 
-    const closet = new Obstacle(130, 0, 50, 65,"closet");
+    const closet = new Obstacle(130, 0, 50, 65, "closet");
     obstacleArr[3] = closet;
-    closet.drawObstacle();    
+    closet.drawObstacle();
 
     var petWidth = 40;
-    var pet = new Pet("dog", canvas.width / 2, canvas.height / 2, petWidth, petWidth/1.1);
-    pet.drawPet1(canvas.width / 2, canvas.height / 2);
+    var pet = new Pet("dog", canvas.width / 2, canvas.height / 2, petWidth, petWidth / 1.1);
+    petWalk(this.x, this.y, dog1IdleUrl);
 
     // play music
     //////////////////// Pet Choice ///////////////////////
